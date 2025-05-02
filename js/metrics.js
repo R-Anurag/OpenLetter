6565fa4d24d54fcadc0009c5190ab6e51e3845b1
Anurag Rai <146112209+R-Anurag@users.noreply.github.com>
2025-05-02 07:06:05 +0530

    Create metrics.js

---
+++ js/metrics.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCkQcooYANlNMtCeQkirONRDKjbZajsIjY",
+  authDomain: "openletter-43e77.firebaseapp.com",
+  projectId: "openletter-43e77",
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
